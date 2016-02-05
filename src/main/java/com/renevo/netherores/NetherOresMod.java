a735393deff3d6b0b2135c9d30b27572efe70c77
Tom Anderson <tanderson@blizzard.com>
2016-02-04 22:17:17 -0800

    Initial Commit

---
+++ src/main/java/com/renevo/netherores/NetherOresMod.java
@@ -0,0 +1,20 @@
+package com.renevo.netherores;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = NetherOresMod.MODID, version = NetherOresMod.VERSION)
+public class NetherOresMod
+{
+    public static final String MODID = "netherores";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
