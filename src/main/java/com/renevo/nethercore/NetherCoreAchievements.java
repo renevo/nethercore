0fbd9ab8fa2460f6d19d2d00992d02c5a0e3115f
Tom Anderson <tanderson@blizzard.com>
2016-02-23 07:14:02 -0800

    Added "Compression" Achievement - Closes #19

--- src/main/java/com/renevo/nethercore/NetherCoreAchievements.java
+++ src/main/java/com/renevo/nethercore/NetherCoreAchievements.java
@@ -8,6 +8,7 @@ import net.minecraft.stats.AchievementList;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public final class NetherCoreAchievements {
 
@@ -15,11 +16,15 @@ public final class NetherCoreAchievements {
     }
 
     public static Achievement netherOreAchievement;
+    public static Achievement compressionAchievement;
 
     public static void init() {
 
         netherOreAchievement = new Achievement("achievement.netherore", "netherore", -2, 10, NetherCoreBlocks.netherOreIron, AchievementList.portal);
         netherOreAchievement.registerStat();
+
+        compressionAchievement = new Achievement("achievement.compressednether", "compressednether", -2, 12, NetherCoreBlocks.blockCompressedNetherrack, netherOreAchievement);
+        compressionAchievement.registerStat();
 
         MinecraftForge.EVENT_BUS.register(new NetherCoreAchievements());
 
@@ -33,4 +38,10 @@ public final class NetherCoreAchievements {
         }
     }
 
+    @SubscribeEvent
+    public void onCraftItem(PlayerEvent.ItemCraftedEvent event) {
+        if (event.crafting.getItem() == NetherCoreBlocks.compressedNetherrackSingle.getItem()) {
+            event.player.addStat(compressionAchievement, 1);
+        }
+    }
 }
